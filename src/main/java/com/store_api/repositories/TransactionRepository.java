0e0306d07c73214c5186c02dc48b80a7cdbf398b
VitienzoCapeletti <vitienzodias@gmail.com>
2025-03-29 22:06:27 -0300

    create transaction repository

---
+++ src/main/java/com/store_api/repositories/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.store_api.repositories;
+
+import com.store_api.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, String> {
+}
