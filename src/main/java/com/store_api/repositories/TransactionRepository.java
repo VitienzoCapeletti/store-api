c475a25ca2f3ef194000ff06fd10319f2abef287
VitienzoCapeletti <vitienzodias@gmail.com>
2025-04-10 23:57:56 -0300

    create repositories

--- src/main/java/com/store_api/repositories/TransactionRepository.java
+++ src/main/java/com/store_api/repositories/TransactionRepository.java
@@ -3,5 +3,5 @@ package com.store_api.repositories;
 import com.store_api.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<Transaction, String> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
--- src/main/java/com/store_api/repositories/UserRepository.java
+++ src/main/java/com/store_api/repositories/UserRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserByDocument(String document);
-    Optional<User> findUserById(String id);
+
+    Optional<User> findUserById(Long id);
 
 }
